121acf8af986952fd59151299f4bc1362040a734
DESKTOP-TMDJO97\Lee <leethornton1992@hotmail.com>
2019-08-03 19:56:58 +0100

    added storie's for filtering points & header with total number of points

---
+++ src/components/filterControls/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "./filterControls.css"
+
+export default class FilterControls extends Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <div className="row bg-warning">
+                    <div className="col-md-12">
+                        <h4>
+                            <span>Filter </span>
+                            <input type="text" placeholder="Point Search" />
+                            <span> Location: </span>
+                            <select id="region">
+                                <option value="all">All</option>
+                                <option value="north">North</option>
+                                <option value="east">East</option>
+                                <option value="west">West</option>
+                                <option value="south">South</option>
+                            </select>
+                        </h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
--- stories/index.js
+++ stories/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import Header from '../src/components/header/';
-import FilterControls from '../src/components/filterControls/';
-import Contact from '../src/components/contact/';
-import ContactList from '../src/components/contactList/';
-import { action } from '@storybook/addon-actions';
-import { MemoryRouter, Route } from "react-router";
+import Header from '../src/components/header/'
+import FilterControls from '../src/components/filterControls/'
+
+storiesOf("Point List App/Header", module).add("default", () => (
+    <Header numPoints={10} />
+));
+
+storiesOf("Point List App/Filter Controls", module).add("default", () => (
+    <FilterControls />
+));
